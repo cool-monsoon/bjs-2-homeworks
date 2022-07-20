5a23360117da2a02ca9522fb9f2d7a56d08504e8
Ольга Храпкова <cool_monsoon@mail.ru>
2022-07-20 16:04:04 +0300

    code optimized

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,17 +1,7 @@
 function compareArrays(arr1, arr2) {
-  let result;
-  if (arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index])) {
-    result = true;
-  } else {
-    result = false
-  }
-  return result;
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
-
-
 function advancedFilter(arr) {
-  let resultArr;
-  resultArr = arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
-  return resultArr;
+  return arr.filter(element => element > 0).filter(element => element % 3 === 0).map(element => element * 10);
 }
