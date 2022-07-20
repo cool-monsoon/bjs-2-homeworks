ceede88a2f1bef6840efc28005ffb96301e33daf
Ольга Храпкова <cool_monsoon@mail.ru>
2022-07-20 16:06:06 +0300

    task 4 done

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,45 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+
+let student1 = new Student("Ronny", "female", 34);
+let student2 = new Student("Anny", "female", 32);
+let student3 = new Student("Jonny", "male", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  }
+  else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...mark) {
+  if (this.marks === undefined) {
+    this.marks = mark;
+  }
+  else {
+    this.marks.push(...mark);
+  }
+}
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+    sum = sum + this.marks[i];
+  }
+  return sum / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete Student.subject;
+  delete Student.marks;
+  this.ecluded = reason;
+}
