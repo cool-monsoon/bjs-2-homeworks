180da9b6fe9e71529345cc374738355ac42aea3c
Ольга Храпкова <cool_monsoon@mail.ru>
2022-07-26 15:07:17 +0300

    Опечатка исправлена

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -22,30 +22,30 @@ class Triangle {
 		this.b = b;
 		this.c = c;
 
-		if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.b + this.c) < this.a){
+		if ((this.a + this.b) < this.c || (this.a + this.c) < this.b || (this.b + this.c) < this.a) {
 			throw new Error("Треугольник с такими сторонами не существует");
+		}
+	}
+
+	getPerimeter() {
+		return this.a + this.b + this.c;
+	}
+
+	getArea() {
+		let p = (this.a + this.b + this.c) / 2;
+		return +(Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c))).toFixed(3);
 	}
 }
 
-getPerimeter(){
-return this.a + this.b +this.c ;
-}
-
-getArea(){
-	let p = (this.a + this.b + this.c) / 2;
-	return +(Math.sqrt(p*(p-this.a)*(p-this.b)*(p-this.c))).toFixed(3);
-}
-}
-
-function getTriangle(a, b, c){
+function getTriangle(a, b, c) {
 	try {
-let triangle1 = new Triangle(a, b, c);
-return triangle1;
-}
-catch(error){
-	return {
-	getArea: () => "Ошибка! Треугольник не существует",
-  getPerimeter: () => "Ошибка! Треугольник не существует"
+		let triangle1 = new Triangle(a, b, c);
+		return triangle1;
 	}
-}
+	catch (error) {
+		return {
+			getArea: () => "Ошибка! Треугольник не существует",
+			getPerimeter: () => "Ошибка! Треугольник не существует"
+		}
+	}
 }
